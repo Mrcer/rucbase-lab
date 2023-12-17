195b0f591dbbbf8f2826ea527e4334177d7884a6
bizt123 <131783931+bizt123@users.noreply.github.com>
2023-12-17 15:56:39 +0800

    Update rm_scan.cpp

--- src/record/rm_scan.cpp
+++ src/record/rm_scan.cpp
@@ -9,6 +9,7 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #include "rm_scan.h"
+
 #include "rm_file_handle.h"
 
 /**
@@ -18,7 +19,8 @@ See the Mulan PSL v2 for more details. */
 RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
     // Todo:
     // 初始化file_handle和rid（指向第一个存放了记录的位置）
-
+    this->rid_ = {.page_no = RM_FIRST_RECORD_PAGE, .slot_no = -1};
+    next();
 }
 
 /**
@@ -27,7 +29,20 @@ RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
 void RmScan::next() {
     // Todo:
     // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
-
+    while (this->rid_.page_no < file_handle_->file_hdr_.num_pages) {
+        RmPageHandle page_handle = file_handle_->fetch_page_handle(this->rid_.page_no);  // 取出rid_对应的页
+        this->rid_.slot_no = Bitmap::next_bit(true, page_handle.bitmap, file_handle_->file_hdr_.num_records_per_page,
+                                              this->rid_.slot_no);  // 在位图中找到最近的，有数据的条目位置
+        if (this->rid_.slot_no < this->file_handle_->file_hdr_.num_records_per_page) {
+            return;  // 条目位置合法则直接返回
+        } else {
+            this->rid_ = Rid{this->rid_.page_no + 1, -1};  // 页号+1，准备遍历下一页
+            if (rid_.page_no >= file_handle_->file_hdr_.num_pages) {
+                rid_ = Rid{RM_NO_PAGE, -1};  // 已经遍历到尽头，直接给rid_赋非法值并退出循环，停止搜索
+                break;
+            }
+        }
+    }
 }
 
 /**
@@ -35,13 +50,10 @@ void RmScan::next() {
  */
 bool RmScan::is_end() const {
     // Todo: 修改返回值
-
-    return false;
+    return rid_.page_no == RM_NO_PAGE;
 }
 
 /**
  * @brief RmScan内部存放的rid
  */
-Rid RmScan::rid() const {
-    return rid_;
-}
+Rid RmScan::rid() const { return rid_; }
